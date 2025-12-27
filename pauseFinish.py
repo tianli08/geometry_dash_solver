6120bd78e2e17fd7ff5deb647d513f9694fba140
Tian Hao Li <tianli081104@gmail.com>
2025-12-27 16:48:53 -0500

    autostart detection complete

--- pauseFinish.py
+++ pauseFinish.py
@@ -7,30 +7,50 @@ class GameRecorder:
     def __init__ (self):
         self.screenResolution = (1920, 1080)
         self.screenIsRecording = False
+        self.playButtomImg = cv2.imread('./images/play_button.jpg', cv2.IMREAD_UNCHANGED)
+        self.cursorPos = None
 
-    def startRecording(self):
+    def startRecording(self): # Starts a recording and automatically starts level based on the pause screen.
         self.screenIsRecording = True
-        
-        # Just for experimentation with window testing
+        trueSizeX, trueSizeY = pyautogui.size()
+
+        # Just for experimentation with window testing, need to setup recording software
         cv2.namedWindow("Show", cv2.WINDOW_NORMAL)
-        cv2.resizeWindow("Show", 1080, 590)
+        cv2.resizeWindow("Show", 1980, 1020)
 
         while True:
             # cv2.resizeWindow("Show", 300, 200)
             img = pyautogui.screenshot()
             frame = np.array(img)
+            ss_h = frame.shape[0]
+            ss_w = frame.shape[1]
+            scale_h = trueSizeY / ss_h
+            scale_w = trueSizeX / ss_w
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # Need frame resizing, changing for proper config on HiDPI, may not need a non version config.
-            frame = cv2.resize(frame, (1080, 590))
+            result = cv2.matchTemplate(frame, self.playButtomImg, cv2.TM_CCOEFF_NORMED)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
-            cv2.imshow('Show', frame)
-            if cv2.waitKey(1) == ord('q'):
+            # Need frame resizing, changing for proper config on HiDPI, may not need a non version config.
+            # frame = cv2.resize(frame, (1280, 720))
+            if max_val >= 0.97:
+                needle_h = self.playButtomImg.shape[0]
+                needle_w = self.playButtomImg.shape[1]
+                self.cursorPos = ((max_loc[0] + needle_w/2) * scale_w  , (max_loc[1] + needle_h/2) * scale_h) # Center of top left coord, and bottom right coord
+                print(max_loc)
+                cv2.rectangle(frame, max_loc, (max_loc[0] + needle_w, max_loc[1] + needle_h), (0, 255, 255), 2)
+                cv2.imshow('Show', frame)
+                pyautogui.click(self.cursorPos) # Clicks when menu button is visible
+            else:
+                cv2.imshow('Show', frame)
+            if cv2.waitKey(1000) == ord('q'): #Lower delay for now
                 break
         cv2.destroyAllWindows()
+        # pyautogui.moveTo(self.cursorPos)
+        # print(self.cursorPos)
 
 
     def pauseButton(self) -> None:
-        print("Button here::::::")
+        print(pyautogui.size())
 
         
 
